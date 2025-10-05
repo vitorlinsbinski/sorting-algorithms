e0b85983ff93e1b1f8780d47f2adcb7f4507d529
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2025-10-05 15:23:02 -0400

    melhorias no quick sort e merge sort

--- quck_sort2.cpp
+++ quck_sort2.cpp
@@ -29,23 +29,36 @@ void swap(int *l, int *r) {
     *r = temp;
 }
 
+int chooseMedianBetweenThree(int *list, int l, int h) {
+    int mid = (l + h) / 2;
+
+    int a = list[l];
+    int b = list[mid];
+    int c = list[h];
+
+    if ((a <= b && b <= c) || (c <= b && b <= a))
+        return mid;
+    else if ((b <= a && a <= c) || (c <= a && a <= b))
+        return l;
+    else
+        return h;
+}
+
 void quickSort(int *list, int n) {
     if (n <= 1) return;
+
+    int pivotIndex = chooseMedianBetweenThree(list, 0, n - 1);
+    swap(&list[0], &list[pivotIndex]); 
 
     int pivot = list[0];
     int i = 1, j = n - 1;
 
     while (true) {
-        while (i < n && list[i] < pivot) {
-            i++;
-        }
-
-        while (j > 0 && list[j] > pivot) {
-            j--;
-        }
+        while (i < n && list[i] < pivot) i++;
+        while (j > 0 && list[j] > pivot) j--;
 
         if (i > j) {
-            swap(&list[0], &list[j]);
+            swap(&list[0], &list[j]); // pivô na posição final
             break;
         } else {
             swap(&list[i], &list[j]);
@@ -54,16 +67,16 @@ void quickSort(int *list, int n) {
         }
     }
 
-    quickSort(list, j);
-    quickSort(list + j + 1, n - j - 1);
+    quickSort(list, j);              // esquerda
+    quickSort(list + j + 1, n - j - 1); // direita
 }
 
 int main() {
-    int n = 20;
+    int n = 50;
     int interval = 1000;
     int *myList = generateRandomList(n, interval);
     printList(myList, n);
-
+    cout << endl;
     quickSort(myList, n);
     printList(myList, n);
     return 0;
